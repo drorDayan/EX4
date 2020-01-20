e2cf498e21a1ee0f47fcddbbbdfe897b49f47de3
drorda <dror.d.dayan@gmail.com>
2020-01-20 19:21:20 +0200

    removed usless code

--- ex42.py
+++ ex42.py
@@ -2,141 +2,29 @@ from arr2_epec_seg_ex import *
 import random
 import time
 from math import sqrt
-from collections import defaultdict
-
 
 # Configurable Variables: #
-
 
 k_nearest = 50
 steer_eta = FT(0.5)
 inflation_epsilon = FT(0.02)
-FREESPACE = 'freespace'
 num_of_points_in_batch = 1000
 use_single_robot_movement = True
+FREESPACE = 'freespace'
 
 # Code: #
-
-
 class RRT_Node():
-    def __init__(self, pt, pr = None, n = 0):
+    def __init__(self, pt, pr=None, n=0):
         self.point = pt
         self.parent = pr
         self.nval = n # Will be used to store distance to this point, etc.
+
     def get_path_to_here(self, ret_path):
         cur = self
         while cur != None:
             ret_path.insert(0, cur.point)# what is that? we need two Point_2 here
             cur = cur.parent
         return ret_path
-
-
-
-# The following function returns the transformed distance between two points
-# (for the squared Euclidean distance the transformed distance is the squared distance)
-def transformed_distance(p1, p2):
-    # TODO make it work for k
-    return max((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1]),
-               (p1[2]-p2[2])*(p1[2]-p2[2])+(p1[3]-p2[3])*(p1[3]-p2[3]))
-
-
-# The following function returns the transformed distance between the query
-# point q and the point on the boundary of the rectangle r closest to q.
-def min_distance_to_rectangle(q, r):
-    min_coord_x = r.min_coord(0)
-    min_coord_y = r.min_coord(1)
-    min_coord_x1 = r.min_coord(2)
-    min_coord_y1 = r.min_coord(3)
-    max_coord_x = r.max_coord(0)
-    max_coord_y = r.max_coord(1)
-    max_coord_x1 = r.max_coord(2)
-    max_coord_y1 = r.max_coord(3)
-    x0 = q[0]
-    y0 = q[1]
-    x1 = q[2]
-    y1 = q[3]
-    if x0 < min_coord_x:
-        x0_diff = min_coord_x - x0
-    elif x0 > max_coord_x:
-        x0_diff = x0 - max_coord_x
-    else:
-        x0_diff = FT(0)
-    if y0 < min_coord_y:
-        y0_diff = min_coord_y - y0
-    elif y0 > max_coord_y:
-        y0_diff = y0 - max_coord_y
-    else:
-        y0_diff = FT(0)
-    if x1 < min_coord_x1:
-        x1_diff = min_coord_x1 - x1
-    elif x1 > max_coord_x1:
-        x1_diff = x1 - max_coord_x1
-    else:
-        x1_diff = FT(0)
-    if y1 < min_coord_y1:
-        y1_diff = min_coord_y1 - y1
-    elif y1 > max_coord_y1:
-        y1_diff = y1 - max_coord_y1
-    else:
-        y1_diff = FT(0)
-    return min(x0_diff*x0_diff+y0_diff*y0_diff,
-               x1_diff * x1_diff + y1_diff * y1_diff)
-
-
-# The following function returns the transformed distance between the query
-# point q and the point on the boundary of the rectangle r furthest to q.
-# (For k-nearest search you may leave the following implementation)
-def max_distance_to_rectangle(q, r):
-    min_coord_x = r.min_coord(0)
-    min_coord_y = r.min_coord(1)
-    min_coord_x1 = r.min_coord(2)
-    min_coord_y1 = r.min_coord(3)
-    max_coord_x = r.max_coord(0)
-    max_coord_y = r.max_coord(1)
-    max_coord_x1 = r.max_coord(2)
-    max_coord_y1 = r.max_coord(3)
-    x0 = q[0]
-    y0 = q[1]
-    x1 = q[2]
-    y1 = q[3]
-    if x0 < min_coord_x:
-        x0_diff = min_coord_x - x0
-    elif x0 > max_coord_x:
-        x0_diff = x0 - max_coord_x
-    else:
-        x0_diff = FT(0)
-    if y0 < min_coord_y:
-        y0_diff = min_coord_y - y0
-    elif y0 > max_coord_y:
-        y0_diff = y0 - max_coord_y
-    else:
-        y0_diff = FT(0)
-    if x1 < min_coord_x1:
-        x1_diff = min_coord_x1 - x1
-    elif x1 > max_coord_x1:
-        x1_diff = x1 - max_coord_x1
-    else:
-        x1_diff = FT(0)
-    if y1 < min_coord_y1:
-        y1_diff = min_coord_y1 - y1
-    elif y1 > max_coord_y1:
-        y1_diff = y1 - max_coord_y1
-    else:
-        y1_diff = FT(0)
-    return max(x0_diff*x0_diff+y0_diff*y0_diff,
-               x1_diff * x1_diff + y1_diff * y1_diff)
-
-
-# The following function returns the transformed distance for a value d (a number)
-# Fo example, if d is a value computed using the Euclidean distance, the transformed distance should be d*d
-def transformed_distance_for_value(d):
-    return d*d  # replace this with your implementation
-
-
-# The following function returns the inverse of the transformed distance for a value d (a number)
-# Fo example, if d is a sqaured distance value then its inverse should be sqrt(d)
-def inverse_of_transformed_distance_for_value(d):
-    return sqrt(d)  # replace this with your implementation
 
 
 def get_batch(robot_num, num_of_points_in_batch, max_x, max_y, min_x, min_y, dest_point):
@@ -170,12 +58,7 @@ def get_square_mid(robot):
 def k_nn(tree, k, query, eps):
     search_nearest = True
     sort_neighbors = True
-    # search = K_neighbor_search(tree, query, k, eps, search_nearest, Euclidean_distance(), sort_neighbors)
     # TODO: Experiment with a custom distance (i.e. max between the two 2D-Euclidean distances, I feel like that makes more sense)
-    my_distance = Distance_python(transformed_distance, min_distance_to_rectangle, \
-                               max_distance_to_rectangle, transformed_distance_for_value, \
-                               inverse_of_transformed_distance_for_value)
-    # search = K_neighbor_search_python(tree, query, k, eps, search_nearest, my_distance, sort_neighbors)
     search = K_neighbor_search(tree, query, k, eps, search_nearest, Euclidean_distance(), sort_neighbors)
     lst = []
     search.k_neighbors(lst)
@@ -255,9 +138,9 @@ def paths_too_close(start_point, target_point, robot_width):
     return False
 
 
-def is_valid_config(arrangement, conf, robot_num):
+def is_valid_config(point_locator, conf, robot_num):
     for j in range(robot_num):
-        if not is_in_free_face(arrangement, Point_2(conf[2*j], conf[2*j+1])):
+        if not is_in_free_face(point_locator, Point_2(conf[2*j], conf[2*j+1])):
             return False
         for k in range(j + 1, robot_num):
             if abs(FT.to_double(conf[2*j] - conf[2*k])) < 1+inflation_epsilon.to_double() and \
@@ -266,8 +149,8 @@ def is_valid_config(arrangement, conf, robot_num):
     return True
 
 
-def path_collision_free(arrangement, robot_num, p1, p2):
-    if not is_valid_config(arrangement, p2, robot_num):
+def path_collision_free(point_locator, robot_num, p1, p2):
+    if not is_valid_config(point_locator, p2, robot_num):
         return False
     max_robot_path_len = FT(0)
     for i in range(robot_num):
@@ -280,17 +163,16 @@ def path_collision_free(arrangement, robot_num, p1, p2):
     curr = [p1[i] for i in range(2*robot_num)]
     for i in range(int(sample_amount.to_double())):
         curr = [sum(x, FT(0)) for x in zip(curr, diff_vec)]
-        if not is_valid_config(arrangement, curr, robot_num):
+        if not is_valid_config(point_locator, curr, robot_num):
             return False
     return True
 
 
-def try_connect_to_dest(graph, arrangement, robot_num, tree, dest_point):
+def try_connect_to_dest(graph, point_locator, robot_num, tree, dest_point):
     nn = k_nn(tree, k_nearest, dest_point, FT(0))
     for neighbor in nn:
-        if path_collision_free(arrangement, robot_num, neighbor[0], dest_point):
+        if path_collision_free(point_locator, robot_num, neighbor[0], dest_point):
             graph[dest_point] = RRT_Node(dest_point, graph[neighbor[0]])
-            # graph.add_edge(neighbor[0], dest_point, 1) # TODO more useful weight?
             return True
     return False
 
@@ -321,31 +203,19 @@ def merge_faces_by_freespace_flag(x, y):
 
 
 def overlay_multiple_arrangements(arrs, face_merge_func):
-    # def empty(x, y):
-    #     return None
-    #
-    # traits = Arr_overlay_traits(empty, empty, empty, empty, empty, empty, empty, empty, empty, face_merge_func)
-
     final_arr = arrs[0]
     for arr in arrs[1:]:
         temp_res = Arrangement_2()
 
         overlay(final_arr, arr, temp_res, Arr_face_overlay_traits(face_merge_func))
-        # overlay(final_arr, arr, temp_res, traits)
         final_arr = temp_res
     return final_arr
 
 
-def locate(arr, point):
-    assert isinstance(arr, Arrangement_2)
-    assert isinstance(point, Point_2)
-    return Arr_naive_point_location(arr).locate(point)
-
-
-def is_in_free_face(arrangement, point):
+def is_in_free_face(point_locator, point):
     face = Face()
     # locate can return a vertex or an edge or a face
-    located_obj = locate(arrangement, point)
+    located_obj = point_locator.locate(point)
     if located_obj.is_vertex():
         return False
     if located_obj.is_halfedge():
@@ -353,13 +223,6 @@ def is_in_free_face(arrangement, point):
     if located_obj.is_face():
         located_obj.get_face(face)
     return face.data()[FREESPACE]
-
-
-def point_2_to_xy(p):
-  return (p.x().to_double(), p.y().to_double())
-
-
-from gui.gui import GUI, QtCore, QtGui, QtWidgets, Qt, QPointF
 
 
 def generate_path(path, robots, obstacles, destination):
@@ -378,6 +241,7 @@ def generate_path(path, robots, obstacles, destination):
     c_space_obstacles = [minkowski_sum_by_full_convolution_2(inflated_square, obs) for obs in cgal_obstacles]
     c_space_arrangements = [polygon_with_holes_to_arrangement(obs) for obs in c_space_obstacles]
     single_arrangement = overlay_multiple_arrangements(c_space_arrangements, merge_faces_by_freespace_flag)
+    point_locator = Arr_naive_point_location(single_arrangement)
 
     max_x, max_y, min_x, min_y = get_min_max(obstacles)
     start_ref_points = [get_square_mid(robot) for robot in robots]
@@ -385,8 +249,6 @@ def generate_path(path, robots, obstacles, destination):
     start_point = Point_d(2*robot_num, sum(start_ref_points, []))
     dest_point = Point_d(2*robot_num, sum(target_ref_points, []))
     vertices = [start_point]
-    print(vertices)
-    # graph = Graph()
     graph = {start_point:RRT_Node(start_point)}
     tree = Kd_tree(vertices)
     while True:
@@ -396,10 +258,8 @@ def generate_path(path, robots, obstacles, destination):
         new_points = []
         for p in batch:
             near = get_nearest(robot_num, tree, new_points, p)
-            # print(near)
             new = steer(robot_num, near, p, steer_eta)
-            # print(new)
-            if path_collision_free(single_arrangement, robot_num, near, new):
+            if path_collision_free(point_locator, robot_num, near, new):
                 new_points.append(new)
                 vertices.append(new)
                 graph[new] = RRT_Node(new, graph[near])
@@ -410,7 +270,7 @@ def generate_path(path, robots, obstacles, destination):
                     new_data.append(new[2 * i + 1])
                     new_data = new_data + [near[j] for j in range(2 * i + 2, 2*robot_num)]
                     my_new = Point_d(2*robot_num, new_data)
-                    if path_collision_free(single_arrangement, robot_num, near, my_new):
+                    if path_collision_free(point_locator, robot_num, near, my_new):
                         new_points.append(my_new)
                         vertices.append(my_new)
                         graph[my_new] = RRT_Node(my_new, graph[near])
@@ -419,19 +279,11 @@ def generate_path(path, robots, obstacles, destination):
         # each double the size of the previous one
         tree.insert(new_points)
         print("vertices amount: "+str(len(vertices)))
-        if try_connect_to_dest(graph, single_arrangement, robot_num, tree, dest_point):
+        if try_connect_to_dest(graph, point_locator, robot_num, tree, dest_point):
             break
-    dpath = []
-    graph[dest_point].get_path_to_here(dpath)
-    for dp in dpath:
-        path.append([Point_2(dp[2*i],dp[2*i+1]) for i in range(robot_num)])
-    # dijk_path = dijkstra(graph, start_point, dest_point)
-    # if len(dijk_path) > 0:
-    #     for pd in dijk_path:
-    #         next_conf = []
-    #         for i in range(robot_num):
-    #             next_conf.append(Point_2(pd[2*i],pd[2*i+1]))
-    #         path.append(next_conf)
-    print(vertices)
+    d_path = []
+    graph[dest_point].get_path_to_here(d_path)
+    for dp in d_path:
+        path.append([Point_2(dp[2*i], dp[2*i+1]) for i in range(robot_num)])
     print("finished, " + "time= " + str(time.time() - start))
 
