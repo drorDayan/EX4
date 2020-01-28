7d7e8990ede1c792a67dece878e29d167baf3d6b
unknown <dror.d.dayan@gmail.com>
2020-01-28 22:16:06 +0200

    hard scenes (for single robot movement tests)

--- ex42.py
+++ ex42.py
@@ -9,7 +9,7 @@ k_nearest = 50
 steer_eta = FT(0.6)
 num_of_points_in_batch = 1200
 single_robot_movement_if_less_then = 20
-use_single_robot_movement = False
+use_single_robot_movement = True
 FREESPACE = 'freespace'
 
 # Code: #
