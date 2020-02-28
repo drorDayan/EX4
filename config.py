ef2665428c4dea48d8b9ab8841b1009da7d12ab6
Nave <navetal@gmail.com>
2020-02-28 15:22:01 +0200

    did some testing

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 # Metrics weights:
 weight_distance = 1
 weight_obstacle_clearance_inv = 0
-weight_robot_clearance_inv = 1
+weight_robot_clearance_inv = 0
+
 # Running time:
-seconds_to_run = 600
-seconds_per_sample = 25
+seconds_to_run = 10
+seconds_per_sample = 1
+
 # Algorithm Variables:
 k_nearest = 10
 steer_eta = 0.6
 num_of_points_in_batch = 1200
+
 # Miscellaneous:
 FREESPACE = 'freespace'
